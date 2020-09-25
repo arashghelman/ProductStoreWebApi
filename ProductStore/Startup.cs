01738f8a2a25f937e3debf6e0f6d790dd2c5be45
Ghelman <arashghelman77@gmail.com>
2020-09-25 10:24:50 +0330

    .

--- ProductStore/Startup.cs
+++ ProductStore/Startup.cs
@@ -6,10 +6,12 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using ProductStore.Models.DomainModels.EF.DTO.POCO;
 
 namespace ProductStore
 {
@@ -26,6 +28,7 @@ namespace ProductStore
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddDbContext<ProductStoreDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("ProductStoreConnectionString")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
