ec4ef51a7a7a0cc1d27771fd1e3a9ed2616bd384
Ghelman <arashghelman77@gmail.com>
2020-09-25 10:24:32 +0330

    Added Ef core Packages. Added and configured Database's Context class.

---
+++ ProductStore/Models/DomainModels/EF/DTO/POCO/ProductStoreDbContext.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace ProductStore.Models.DomainModels.EF.DTO.POCO
+{
+    public class ProductStoreDbContext : DbContext
+    {
+        #region [- ctor -]
+
+        public ProductStoreDbContext(DbContextOptions<ProductStoreDbContext> options)
+                : base(options)
+        {
+        }
+        #endregion
+
+        #region [- OnConfiguring(DbContextOptionsBuilder optionsBuilder) -]
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+        }
+        #endregion
+
+        #region [- OnModelCreating(ModelBuilder modelBuilder) -]
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+        #endregion
+    }
+}
