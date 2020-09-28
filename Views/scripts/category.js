ad096ac3c6a1df0b9206f06dee3450c416786871
Ghelman <arashghelman77@gmail.com>
2020-09-28 17:00:25 +0330

    tweaked the html page

--- Views/scripts/category.js
+++ Views/scripts/category.js
@@ -3,10 +3,16 @@ function sendAjaxRequest(method, url, data) {
         type: method,
         url: url,
         data: data ? JSON.stringify(data) : null,
-        dataType: "application/json"
+        dataType: "application/json",
+        error: response => console.log(response)
     });
 }
 
 $(document).ready(function () {
-    sendAjaxRequest('Get', 'https://localhost:5001/wapi/category/get');
-});
+    //sendAjaxRequest('Get', 'https://localhost:5001/wapi/category/get');
+
+    $(document).on('input', '#units-range', function() {
+        $('#value-span').html( $(this).val() );
+    });
+ 
+}); 
